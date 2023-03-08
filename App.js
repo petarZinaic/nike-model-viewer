31d6c7339a251f92e20ad6cfbb3e3e65e3dfbd0e
petar-zinaic <petar.zinaic@marbleit.rs>
2023-03-08 12:27:01 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEngine, EngineView } from '@babylonjs/react-native';
+import { Scene } from "@babylonjs/core";
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
+  const engine = useEngine();
+  const [camera, setCamera] = useState();
+
+  useEffect(() => {
+    if(engine) {
+      const scene = new Scene(engine);
+      scene.createDefaultCamera(true);
+      setCamera(scene.activeCamera);
+    }
+  }, [engine])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <EngineView style={styles.engineView} camera={camera}/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  engineView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
